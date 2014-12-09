754266536d52259e32fb71f8ff71fe0377e9e473
Swift <dbarnett@uccs.edu>
2014-12-09 15:29:24 -0700

    Enumerations
    Whoooooo

---
+++ GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/GrandTheftCandy/GameStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GrandTheftCandy
+{
+    public enum GameStates
+    {
+        MainMenu,
+        PauseMenu,
+        HelpMenu,
+        Play,
+        Win,
+        Lose
+    }
+}
